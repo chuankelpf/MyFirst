1f4094a02aa45b612ca59eea2185dd845857b698
南京单聊李 <lipengfei@liushui.local>
2017-12-03 22:25:01 +0800

    no message

--- Layer/RootViewController.m
+++ Layer/RootViewController.m
@@ -20,6 +20,7 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
     NSLog(@"你好世界你是最美的");
+      NSLog(@"你好世界你是最美的22");
 }
 - (IBAction)wwww:(UIButton *)sender {
     CustomerView *cust = [[CustomerView alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
